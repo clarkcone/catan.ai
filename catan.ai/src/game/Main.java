4c0f9756df630896fd15b54deb652f01538ead86
clarkcone <clark.j.cone@gmail.com>
2019-04-21 19:15:36 -0400

    more directory setup

--- catan.ai/src/game/Main.java
+++ catan.ai/src/game/Main.java
@@ -1,10 +1,9 @@
-package catan;
+package game;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello Catan!");
 	}
 
 }
